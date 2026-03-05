57e6806b7d1cb7c12dad0e9b09dcd8078a235e62
VytorVilar <vytorplay5050@gmail.com>
2026-03-05 16:25:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Importar Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Configuração do seu Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBcvHd0Kg_hkur0X85pIkkSUljEd4nAROQ",
   authDomain: "cyber-tracker-b1822.firebaseapp.com",
@@ -12,34 +10,36 @@ const firebaseConfig = {
   appId: "1:640630013098:web:5252369586ab63d5f6a4d3"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-
-// Conectar com banco de dados
 const db = getFirestore(app);
 
-// Função para coletar dados
-async function coletarDados() {
+async function registrarVisita(){
 
-  let ipData = await fetch("https://ipapi.co/json/");
-  ipData = await ipData.json();
+  try{
 
-  const visitante = {
-    ip: ipData.ip,
-    pais: ipData.country_name,
-    cidade: ipData.city,
-    provedor: ipData.org,
-    navegador: navigator.userAgent,
-    sistema: navigator.platform,
-    idioma: navigator.language,
-    resolucao: screen.width + "x" + screen.height,
-    data: new Date().toString()
-  };
+    let ipData = await fetch("https://ipapi.co/json/");
+    ipData = await ipData.json();
 
-  // salvar no Firebase
-  await addDoc(collection(db, "visitas"), visitante);
+    const visitante = {
+      ip: ipData.ip,
+      cidade: ipData.city,
+      pais: ipData.country_name,
+      provedor: ipData.org,
+      navegador: navigator.userAgent,
+      sistema: navigator.platform,
+      idioma: navigator.language,
+      resolucao: screen.width + "x" + screen.height,
+      data: serverTimestamp()
+    };
 
-  console.log("Visitante registrado", visitante);
+    await addDoc(collection(db, "visitas"), visitante);
+
+    console.log("Nova visita registrada");
+
+  }catch(e){
+    console.error("Erro ao registrar visita", e);
+  }
+
 }
 
-coletarDados();
+registrarVisita();
