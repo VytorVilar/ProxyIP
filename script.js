c7e265352298314b3466492a65b36b889542fffa
VytorVilar <vytorplay5050@gmail.com>
2026-03-05 16:31:55 -0300

    script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcvHd0Kg_hkur0X85pIkkSUljEd4nAROQ",
-  authDomain: "cyber-tracker-b1822.firebaseapp.com",
-  projectId: "cyber-tracker-b1822",
-  storageBucket: "cyber-tracker-b1822.firebasestorage.app",
-  messagingSenderId: "640630013098",
-  appId: "1:640630013098:web:5252369586ab63d5f6a4d3"
+apiKey: "AIzaSyBcvHd0Kg_hkur0X85pIkkSUljEd4nAROQ",
+authDomain: "cyber-tracker-b1822.firebaseapp.com",
+projectId: "cyber-tracker-b1822",
+storageBucket: "cyber-tracker-b1822.firebasestorage.app",
+messagingSenderId: "640630013098",
+appId: "1:640630013098:web:5252369586ab63d5f6a4d3"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -15,30 +15,47 @@ const db = getFirestore(app);
 
 async function registrarVisita(){
 
-  try{
+try{
 
-    let ipData = await fetch("https://ipapi.co/json/");
-    ipData = await ipData.json();
+let ipData = await fetch("https://ipapi.co/json/");
+ipData = await ipData.json();
 
-    const visitante = {
-      ip: ipData.ip,
-      cidade: ipData.city,
-      pais: ipData.country_name,
-      provedor: ipData.org,
-      navegador: navigator.userAgent,
-      sistema: navigator.platform,
-      idioma: navigator.language,
-      resolucao: screen.width + "x" + screen.height,
-      data: serverTimestamp()
-    };
+document.getElementById("ip").textContent = ipData.ip;
+document.getElementById("pais").textContent = ipData.country_name;
+document.getElementById("cidade").textContent = ipData.city;
+document.getElementById("provedor").textContent = ipData.org;
 
-    await addDoc(collection(db, "visitas"), visitante);
+document.getElementById("sistema").textContent = navigator.platform;
+document.getElementById("navegador").textContent = navigator.userAgent;
+document.getElementById("idioma").textContent = navigator.language;
+document.getElementById("resolucao").textContent = screen.width + "x" + screen.height;
 
-    console.log("Nova visita registrada");
+const visitante = {
 
-  }catch(e){
-    console.error("Erro ao registrar visita", e);
-  }
+ip: ipData.ip,
+pais: ipData.country_name,
+cidade: ipData.city,
+provedor: ipData.org,
+sistema: navigator.platform,
+navegador: navigator.userAgent,
+idioma: navigator.language,
+resolucao: screen.width + "x" + screen.height,
+url: window.location.href,
+referer: document.referrer,
+data: serverTimestamp()
+
+};
+
+await addDoc(collection(db,"visitas"), visitante);
+
+document.getElementById("status").innerText = "Visitante registrado no banco";
+
+}catch(e){
+
+document.getElementById("status").innerText = "Erro ao registrar visitante";
+console.error(e);
+
+}
 
 }
 
